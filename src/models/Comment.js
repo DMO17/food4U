57ad4aecc93d564a37313aa400a65ac5524d5afd
Dahir Mohamed <dmoabdi98@gmail.com>
2021-12-25 18:12:59 +0000

    comment model complete

---
+++ src/models/Comment.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("../config/connection");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    // primaryKey: true,
+    autoIncrement: true,
+  },
+  comment: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "user",
+      key: "id",
+    },
+    onDelete: "cascade",
+  },
+  post_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "post",
+      key: "id",
+    },
+    onDelete: "cascade",
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableName: true,
+  underScored: true,
+  moduleName: "comment",
+};
+
+class Comment extends Model {}
+
+Comment.init(schema, options);
+
+module.exports = Comment;
