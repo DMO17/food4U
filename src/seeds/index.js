9c508b83911456709b357e98ff6e412ede203311
Dahir Mohamed <dmoabdi98@gmail.com>
2021-12-25 21:36:38 +0000

    able to seed user table with 2 users

--- src/seeds/index.js
+++ src/seeds/index.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const connection = require("../config/connection");
+const seedUsers = require("./user-seeds");
+
+const seedAll = async () => {
+  await connection.sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+
+  await seedUsers();
+  console.log("\n----- USERS SEEDED -----\n");
+
+  //   await seedProducts();
+  //   console.log("\n----- PRODUCTS SEEDED -----\n");
+
+  //   await seedTags();
+  //   console.log("\n----- TAGS SEEDED -----\n");
+
+  //   await seedProductTags();
+  //   console.log("\n----- PRODUCT TAGS SEEDED -----\n");
+
+  process.exit(0);
+};
+
+seedAll();
---
+++ src/seeds/user-seeds.js
@@ -0,0 +1,35 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    first_name: "Aaron",
+    last_name: "Rodgers",
+    username: "qb-packers12",
+    email: "aaron12@gmail.com",
+    password: "imabadman12",
+    // phone_num: 12345678910,
+    city: "London",
+    address: "12 Earlslane n23 6xy ",
+    profile_url:
+      "https://whatsondisneyplus.com/wp-content/uploads/2021/09/bart-.png",
+    description:
+      "Bart is the mischievous, rebellious, misunderstood, disruptive and potentially dangerous oldest child.",
+  },
+  {
+    first_name: "Patrick",
+    last_name: "Mahamoes",
+    username: "mahomeboy15",
+    email: "15kcpat@gmail.com",
+    password: "kelcetyreek",
+    // phone_num: 15345678918,
+    city: "Manchester",
+    address: "345 Chester avenue m16 7wk ",
+    // profile_url: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
