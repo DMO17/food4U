63a3c76467e4db1895bcec6766051e0b45ff6130
Dahir Mohamed <dmoabdi98@gmail.com>
2021-12-25 18:04:12 +0000

    post module complete

---
+++ src/models/Post.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("../config/connection");
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  food_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  food_url: {
+    type: DataTypes.STRING,
+    defaultValue:
+      "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "user",
+      key: "id",
+    },
+    onDelete: "cascade",
+  },
+  food_type_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "food_type",
+      key: "id",
+    },
+    onDelete: "cascade",
+  },
+  //status enum
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  freezeTableName: true,
+  underScored: true,
+  moduleName: "post",
+};
+
+class Post extends Model {}
+
+Post.init(schema, options);
+
+module.exports = Post;
