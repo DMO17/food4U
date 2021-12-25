f118e720aa4dc35a0d00a355a3d4658afbc70acb
Dahir Mohamed <dmoabdi98@gmail.com>
2021-12-25 17:26:22 +0000

    created a simple server that connects to sql db

--- src/server.js
+++ src/server.js
@@ -1,1 +1,29 @@
-console.log("hello");
+// npm packages import
+const express = require("express");
+
+// global imports
+const connection = require("./config/connection");
+
+// setting up server
+
+const PORT = 4000;
+
+const app = express();
+
+const init = async () => {
+  try {
+    await connection.sync({ force: false });
+
+    console.log(`[INFO] :  Connection to DATABASE is successful`);
+
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(
+      `[ERROR]: Connection to DATABASE has failed - ${error.message}`
+    );
+  }
+};
+
+init();
