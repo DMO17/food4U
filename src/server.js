e5b7f3440e257fd2896ea9b9bf85345990613fc6
Dahir Mohamed <dmoabdi98@gmail.com>
2022-01-07 22:38:28 +0000

    conneected a session to the server

--- src/server.js
+++ src/server.js
@@ -2,24 +2,44 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 // global imports
 const connection = require("./config/connection");
-
 const routes = require("./routes");
 
 // setting up server
 
 const PORT = 4000;
-
 const app = express();
 
-const hbs = expressHandlebars.create({});
+//session
+const sessionOptions = {
+  secret: "Super secret secret",
+  cookie: {
+    // Stored in milliseconds (86400 === 1 day)
+    maxAge: 8640000,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: connection,
+  }),
+};
 
+app.use(session(sessionOptions));
+
+//handlebars
+const hbs = expressHandlebars.create({});
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+//middleware
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
 const init = async () => {
   try {
     await connection.sync({ force: false });
