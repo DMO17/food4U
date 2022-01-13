e320ab5bbe4f9665f1c7ff0898bda2f0df1e61ef
Asmaa <Asmaa99musse@gmail.com>
2022-01-13 19:03:56 +0000

    create food  post api controller

--- src/controller/api/food-post.js
+++ src/controller/api/food-post.js
@@ -1,33 +1,60 @@
-const { getPayloadWithValidFieldsOnly } = require("../../helper");
+const {
+  getPayloadWithValidFieldsOnly,
+  isAllRequiredFieldsPresent,
+} = require("../../helper");
 const { Post } = require("../../models");
 
 const createFoodPost = async (req, res) => {
-  const validPostBodyFields = getPayloadWithValidFieldsOnly(
-    [
-      "food_name",
-      "description",
-      "food_url",
-      "price",
-      "location",
-      "food_type",
-      "item",
-      "status",
-    ],
-    req.body
-  );
+  const errorMessage = "Failed to create food post";
+  try {
+    const validPostBodyFields = getPayloadWithValidFieldsOnly(
+      [
+        "food_name",
+        "description",
+        "food_url",
+        "price",
+        "location",
+        "food_type",
+        "item",
+        "status",
+      ],
+      req.body
+    );
 
-  if (Objects.keys(validPostBodyFields).length != 8) {
-    return res.json({
+    const checkAllRequiredField = isAllRequiredFieldsPresent(
+      [
+        "food_name",
+        "description",
+        "food_url",
+        "price",
+        "location",
+        "food_type",
+      ],
+      validPostBodyFields
+    );
+
+    if (!checkAllRequiredField) {
+      console.log(
+        `[ERROR]: ${errorMessage} | Please Provide The Correct required Post Body Fields`
+      );
+      return res.status(500).json({
+        success: false,
+        message: errorMessage,
+      });
+    }
+
+    const payLoad = { user_id: req.session.user.id, ...validPostBodyFields };
+
+    const foodPost = await Post.create(payLoad);
+
+    res.json({ data: foodPost });
+  } catch (error) {
+    console.log(`[ERROR]: ${errorMessage} | ${error.message}`);
+    return res.status(500).json({
       success: false,
-      message: "Please Provide The Correct Post Body Fields",
+      message: errorMessage,
     });
   }
-
-  const payLoad = { user_id: req.session.user.id, ...validPostBodyFields };
-
-  const foodPost = await Post.create(payLoad);
-
-  res.json({ data: foodPost });
 };
 
 const updateFoodPostById = (req, res) => {
