172d4304a4870e77b2108f15c4e53102ecf2a5f4
Dahir Mohamed <dmoabdi98@gmail.com>
2022-01-15 13:07:01 +0000

    account pages template completed

--- src/routes/view/privateRoute.js
+++ src/routes/view/privateRoute.js
@@ -20,9 +20,9 @@ router.use("/food-post/view/:postId", renderFoodPostById);
 router.use("/food-post/create", renderFoodPostForm);
 router.use("/food-post/:postId/order", renderOrderForm);
 
-router.use("/profile", renderProfilePage);
 router.use("/profile/posts", renderProfilePosts);
 router.use("/profile/orders", renderProfileOrders);
+router.use("/profile", renderProfilePage);
 
 router.use("/watch-list", renderWatchList);
 
